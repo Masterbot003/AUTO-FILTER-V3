34009e52fd0aa139b3c179435e03493499beb871
SOUL PC <114959852+Masterbot003@users.noreply.github.com>
2024-01-24 22:41:10 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -1,17 +1,19 @@
 import pymongo
-from info import DATABASE_URL, DATABASE_NAME
+from info import DATABASE_URI, DATABASE_NAME
 from pyrogram import enums
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URL)
-mydb = myclient["GlobalFilters"]
+myclient = pymongo.MongoClient(DATABASE_URI)
+mydb = myclient[DATABASE_NAME]
 
 
 
 async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
     mycol = mydb[str(gfilters)]
+    # mycol.create_index([('text', 'text')])
+
     data = {
         'text':str(text),
         'reply':str(reply_text),
@@ -19,6 +21,7 @@ async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
         'file':str(file),
         'alert':str(alert)
     }
+
     try:
         mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
     except:
